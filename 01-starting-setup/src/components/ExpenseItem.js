83be051e0f5759999fc3a1f1fead2fd115f1bc43
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-06-17 22:29:13 +0200

    udemy course initial setup

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  //1 root element per return statement
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$249.67$</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
