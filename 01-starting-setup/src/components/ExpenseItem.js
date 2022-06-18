13038b9d22170af5f5a8f467f9c553b3f28adef0
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-06-18 19:36:18 +0200

    date formatting

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -2,13 +2,18 @@ import './ExpenseItem.css';
 
 //props is an object which holds all the values we get for the attributes on our elements inside App for example
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2022, 2, 28);
-  // const expenseTitle = 'Car Insurance';
-  // const expenseAmount = 294.67;
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   //1 root element per return statement
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
