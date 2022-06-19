37f7d643baeb2193927e5c0e0299cf2b0ec6d50a
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-06-19 18:14:28 +0200

    Introduced states and events

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,30 @@
+import { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-//props is an object which holds all the values we get for the attributes on our elements inside App for example
+//props is an object which holds all the values we get for the attributes
+//on our elements inside App for example
 const ExpenseItem = (props) => {
+  //It's one of the react hooks and must only be called
+  //inside react component functions. It returns an array with two elements always
+  //with the initialised value and the updating function
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log('Clicked!');
+  };
   //1 root element per return statement
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
