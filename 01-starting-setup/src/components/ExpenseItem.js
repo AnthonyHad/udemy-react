e1a78131798fa12440f6c8733236adecaac4fa4f
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-06-19 15:02:30 +0200

    added date component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,19 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 //props is an object which holds all the values we get for the attributes on our elements inside App for example
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   //1 root element per return statement
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
