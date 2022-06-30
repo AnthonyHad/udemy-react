adfb41f697d8d93abd6e9d524bd1572f6f18b27f
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-06-30 23:28:21 +0200

    added refs

--- 03-practice-project-1/src/components/Users/UserForm.js
+++ 03-practice-project-1/src/components/Users/UserForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -7,28 +7,34 @@ import Wrapper from '../Helpers/Wrapper';
 import styles from './UserForm.module.css';
 
 const UserForm = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  // const [enteredUsername, setEnteredUsername] = useState('');
+  // const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const submitHandler = (event) => {
+    //refs obsolete states
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = nameInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age',
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'please enter a valide age (>10)',
@@ -36,9 +42,12 @@ const UserForm = (props) => {
       return;
     }
     // console.log(enteredAge, enteredUsername);
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    props.onAddUser(enteredName, enteredUserAge);
+    //Refs manipulation should be done rarely
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+    // setEnteredUsername('');
+    // setEnteredAge('');
   };
 
   const errorHandler = () => {
@@ -62,8 +71,9 @@ const UserForm = (props) => {
               <input
                 id="username"
                 type="text"
-                value={enteredUsername}
-                onChange={usernameChangeHandler}
+                // value={enteredUsername}
+                // onChange={usernameChangeHandler}
+                ref={nameInputRef}
               />
             </div>
             <div className={styles['new-user__control']}>
@@ -72,8 +82,9 @@ const UserForm = (props) => {
                 id="age"
                 type="number"
                 step="1"
-                value={enteredAge}
-                onChange={ageChangeHandler}
+                // value={enteredAge}
+                // onChange={ageChangeHandler}
+                ref={ageInputRef}
               />
             </div>
             <div className={styles['new-user__actions']}>
