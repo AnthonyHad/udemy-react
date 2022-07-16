4c35bd5b2a66bc2e13d6987773017ff8436e8b86
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-07-16 12:12:48 +0200

    added form for meals

--- 05-Food-App-project/src/components/Meals/MealItem/MealItem.js
+++ 05-Food-App-project/src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,5 @@
+import MealItemForm from './MealItemForm';
+
 import styles from './MealItem.module.css';
 
 const Mealitem = (props) => {
@@ -9,7 +11,9 @@ const Mealitem = (props) => {
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
