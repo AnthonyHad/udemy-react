bb54366a3aae3c8990b4ca7e402c9ab26118b12d
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-08-26 08:46:53 +0200

    added a cart reducer to manage state

--- 05-Food-App-project/src/store/CartProvider.js
+++ 05-Food-App-project/src/store/CartProvider.js
@@ -1,19 +1,47 @@
 //Used to manage the context data and provide this data to the components that need it
+import { useReducer } from 'react';
 
 import CartContext from './cart-context';
 
-const addItemToCartHandler = (item) => {};
-
-const removeItemFromCartHandler = (id) => {};
-
-const cartContext = {
+const defaultCartState = {
   items: [],
   totalAmount: 0,
-  addItem: addItemToCartHandler,
-  removeItem: removeItemFromCartHandler,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    //important to update the state in an immutable way hence the use of concat
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
 };
 
 const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
