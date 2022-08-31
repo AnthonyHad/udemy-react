174aad2576486d69f377a551cf8a7f314fe85fa4
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-08-31 23:20:29 +0200

    added use effect and callback for better api calls

--- 07-Sending-HTTP-Requests/src/App.js
+++ 07-Sending-HTTP-Requests/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,8 +8,9 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
+    setError(null);
     try {
       const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
@@ -30,7 +31,13 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  //Good to be used when we don't want a function to be called on each render cycle to avoid
+  // infinit loop
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies.</p>;
   if (movies.length > 0) {
