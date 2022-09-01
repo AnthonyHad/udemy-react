eff024e129a7ae96730bfef0634ed42b2b84bd68
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-09-02 00:44:07 +0200

    refactored code

--- 08-Custom-Hooks/src/hooks/use-counter.js
+++ 08-Custom-Hooks/src/hooks/use-counter.js
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react';
 
 // has to start with use it's a hard rule
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 };
