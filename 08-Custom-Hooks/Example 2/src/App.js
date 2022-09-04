d761429b432db5aa4467c458f9de22ca44baa5a4
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-09-04 14:05:52 +0200

    adjusting custom hook logic

--- 08-Custom-Hooks/Example 2/src/App.js
+++ 08-Custom-Hooks/Example 2/src/App.js
@@ -7,28 +7,23 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (tasksObj) => {
-    const loadedTasks = [];
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-    setTasks(loadedTasks);
-  };
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: 'https://react-http-b1771-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-    },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-    //adding fetchTasks as a dependancy would induce an infinite loop
-  }, []);
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      {
+        url: 'https://react-http-b1771-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
