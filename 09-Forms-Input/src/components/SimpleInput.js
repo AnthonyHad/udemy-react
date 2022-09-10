733973a09eded43990e37f79a6527507e0fd4f7b
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-09-10 20:14:46 +0200

    added form submission user input handling

--- 09-Forms-Input/src/components/SimpleInput.js
+++ 09-Forms-Input/src/components/SimpleInput.js
@@ -1,9 +1,37 @@
+import { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  //binding the below function to onChange will return the event object
+  //The choice between state and ref depends on what we would like to achieve
+  //If we are interested once then the ref is better
+  //if we need the value after every keystroke(instant validation) => state
+  // if we'd like to rest the enteredInput => State
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    //nameInputRef.current.value = '' => Not ideal, manipulates DOM
+    //setEnteredName('');
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          // value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
