f2dc7014fa7fa737ac8487acb56cb75d1681000d
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-09-11 09:23:59 +0200

    added basic validation

--- 09-Forms-Input/src/components/SimpleInput.js
+++ 09-Forms-Input/src/components/SimpleInput.js
@@ -3,27 +3,40 @@ import { useState, useRef } from 'react';
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   //binding the below function to onChange will return the event object
   //The choice between state and ref depends on what we would like to achieve
   //If we are interested once then the ref is better
   //if we need the value after every keystroke(instant validation) => state
-  // if we'd like to rest the enteredInput => State
+  // if we'd like to reset the enteredInput => State
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
     //nameInputRef.current.value = '' => Not ideal, manipulates DOM
     //setEnteredName('');
   };
+
+  const nameInputClasses = enteredNameIsValid
+    ? 'form-control'
+    : 'form-control invalid';
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -32,6 +45,9 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           // value={enteredName}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
