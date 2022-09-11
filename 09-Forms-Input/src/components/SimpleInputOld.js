7c5c8fc706c2251aa3bbd3f66ba858d3c002a921
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-09-11 12:17:10 +0200

    added custom hook for name validation

--- 09-Forms-Input/src/components/SimpleInputOld.js
+++ 09-Forms-Input/src/components/SimpleInputOld.js
@@ -1,19 +1,22 @@
 import { useState } from 'react';
 
-import useInput from '../hooks/use-input';
-
 const SimpleInput = (props) => {
-  const {
-    value: enteredName,
-    isValid: enteredNameIsValid,
-    hasError: nameInputHasError,
-    valueChangeHandler: nameChangedHandler,
-    inputBlurHandler: nameBlurHandler,
-    reset: resetNameInput,
-  } = useInput((value) => value.trim() !== '');
-
+  // const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
   const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
   const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+  // const [formIsValid, setFormIsValid] = useState(false);
+
+  //binding the below function to onChange will return the event object
+  //The choice between state and ref depends on what we would like to achieve
+  //If we are interested once then the ref is better
+  //if we need the value after every keystroke(instant validation) => state
+  // if we'd like to reset the enteredInput => State
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const emailReg = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
   const enteredEmailIsValid = emailReg.test(enteredEmail.trim()) === true;
@@ -23,6 +26,23 @@ const SimpleInput = (props) => {
   if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true;
   }
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+    //cannot use entered name in the condition as the states are usually scheduled by react
+    // and we will be using an old one
+    // if (event.target.value.trim() !== '') {
+    //   setEnteredNameIsValid(true);
+    // }
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+
+    // if (enteredName.trim() === '') {
+    //   setEnteredNameIsValid(false);
+    // }
+  };
 
   const emailInputChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
@@ -34,6 +54,7 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
+    setEnteredNameTouched(true);
     setEnteredEmailTouched(true);
 
     if (!enteredNameIsValid) {
@@ -45,12 +66,16 @@ const SimpleInput = (props) => {
     }
 
     console.log(enteredName, enteredEmail);
-    resetNameInput();
+    // const enteredValue = nameInputRef.current.value;
+    // console.log(enteredValue);
+    //nameInputRef.current.value = '' => Not ideal, manipulates DOM
+    setEnteredName('');
+    setEnteredNameTouched(false);
     setEnteredEmail('');
     setEnteredEmailTouched(false);
   };
 
-  const nameInputClasses = nameInputHasError
+  const nameInputClasses = nameInputIsInvalid
     ? 'form-control invalid'
     : 'form-control ';
 
@@ -63,13 +88,14 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
+          // ref={nameInputRef}
           type="text"
           id="name"
-          onChange={nameChangedHandler}
-          onBlur={nameBlurHandler}
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
-        {nameInputHasError && (
+        {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty</p>
         )}
       </div>
