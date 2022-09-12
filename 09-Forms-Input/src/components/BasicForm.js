d565c088b0b005f36b13af7dc43f519cd96dfe8f
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-09-12 22:24:49 +0200

    applied learnings to basic form

--- 09-Forms-Input/src/components/BasicForm.js
+++ 09-Forms-Input/src/components/BasicForm.js
@@ -1,21 +1,56 @@
+import { useState } from 'react';
+
 const BasicForm = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [bluredName, setBluredName] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && bluredName;
+
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const bluredNameHandler = (event) => {
+    setBluredName(true);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (!enteredNameIsValid) {
+      return;
+    }
+    console.log(enteredName);
+    setEnteredName('');
+    setBluredName(false);
+  };
+
+  const nameClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="control-group">
+        <div className={nameClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={nameChangeHandler}
+            onBlur={bluredNameHandler}
+          />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
