c7f4c00ab62f95e8efe116b50cd19aae5ff79473
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-09-22 20:33:32 +0200

    added NavLink and some styling

---
+++ 12-React-Router/src/components/MainHeader.js
@@ -0,0 +1,27 @@
+// It allows us to rendre acnhor tags that will listen to clicks,
+// prevent default and update the url for us ("fake navigation")
+import { NavLink } from 'react-router-dom';
+
+import classes from './MainHeader.module.css';
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to="/welcome">
+              Welcome
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/products">
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default MainHeader;
