2ce16a87eb8586f16da7ccb5576b2a6179f561ef
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-09-25 12:40:55 +0200

    added http requests

--- 12-React-Router/Project/src/pages/AllQuotes.js
+++ 12-React-Router/Project/src/pages/AllQuotes.js
@@ -1,25 +1,41 @@
-import QuoteList from '../components/quotes/QuoteList';
+import { useEffect } from 'react';
 
-const DUMMY_QUOTES = [
-  {
-    id: 'q1',
-    author: 'Anthony',
-    text: 'Learning React is super duper fun!',
-  },
-  {
-    id: 'q2',
-    author: 'Max',
-    text: 'Learning React is super duper nuper fun!',
-  },
-  {
-    id: 'q3',
-    author: 'Jo',
-    text: 'Learning React is super duper nuper super fun!',
-  },
-];
+import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
 
 const AllQuotes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default AllQuotes;
