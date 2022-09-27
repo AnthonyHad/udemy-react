bbb2ba6e0b4000c3543712c43fec05e7220657fa
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-09-27 22:17:04 +0200

    modified routes to adapt to 6.4 and used fetching feature with loader

--- 12-React-Router/React Router v6/Routerv6.4/src/pages/BlogPosts.jsx
+++ 12-React-Router/React Router v6/Routerv6.4/src/pages/BlogPosts.jsx
@@ -1,36 +1,23 @@
 import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 import Posts from '../components/Posts';
 import { getPosts } from '../util/api';
 
 function BlogPostsPage() {
-  const [error, setError] = useState();
-  const [posts, setPosts] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function loadPosts() {
-      setIsLoading(true);
-      try {
-        const posts = await getPosts();
-        setPosts(posts);
-      } catch (err) {
-        setError(err.message);
-      }
-      setIsLoading(false);
-    }
-
-    loadPosts();
-  }, []);
-
+  const loaderData = useLoaderData();
   return (
     <>
       <h1>Our Blog Posts</h1>
-      {isLoading && <p>Loading posts...</p>}
-      {error && <p>{error}</p>}
-      {!error && posts && <Posts blogPosts={posts} />}
+      <Posts blogPosts={loaderData} />
     </>
   );
 }
 
 export default BlogPostsPage;
+
+//This function is used inside the App folder which contains our routes
+// and is passed as a prop to the route which gets us here and then is passed to the component
+export function loader() {
+  return getPosts();
+}
