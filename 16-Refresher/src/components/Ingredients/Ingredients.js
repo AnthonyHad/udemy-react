712d70770ef9e87dfc0b535626f4a13708381938
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-10-31 00:13:37 +0100

    added use effect hook to get ingredients

--- 16-Refresher/src/components/Ingredients/Ingredients.js
+++ 16-Refresher/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,6 +6,25 @@ import Search from './Search';
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
+
+  //Runs after and every render cycle
+  useEffect(() => {
+    fetch(
+      'https://react-http-b1771-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json'
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setIngredients(loadedIngredients);
+      });
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch(
