3d4f248ce0a647390c9b268642b1a5669f96d81f
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-10-31 14:23:20 +0100

    Added use context hook

--- 16-Refresher/src/index.js
+++ 16-Refresher/src/index.js
@@ -3,6 +3,11 @@ import * as ReactDOMClient from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import AuthContextProvider from './context/auth-context';
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
